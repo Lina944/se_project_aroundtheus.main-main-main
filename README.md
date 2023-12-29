2fbee8a5c3d66dbf5ae5a952f878e9afc50169a9
Tsvetelina Stefanova <tsvetelinastefanova944@gmail.com>
2023-12-29 16:00:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Around the U.S.
+
+The "Around the U.S." Interactive webpade is the third required project in the Sofware Engineering program at TripleTen. The webpage was created using HTML and CSS, based on Figma design. The project was made to demonstrate the ability to make responsibe webpade which can fit to different screen sizes. Enjoy!
+
+## Project features
+
+- Semantic HTML5
+- Flexbox
+- Positioning
+- Grid Layout
+
+Link to the deployed site via GitHub Pages: https://lina944.github.io/se_project_aroundtheus.main-main-main/
+
---
+++ scripts/index.js
@@ -0,0 +1,151 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+
+
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+
+const cardsWrap = document.querySelector(".cards__list");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const addCardModal = document.querySelector("#add-card-modal");
+const profileEditForm = profileEditModal.querySelector("#modal-form");
+
+const addCardFormElement = addCardModal.querySelector("#card-form");
+
+const modalPreviewImage = document.querySelector("#modal-image-preview");
+const modalImagePreviewLink = modalPreviewImage.querySelector(
+  ".modal__preview-image"
+);
+const modalPreviewTitle = modalPreviewImage.querySelector(
+  ".modal__title-preview"
+);
+
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileCloseModalButton = profileEditModal.querySelector(
+  "#modal-close-button"
+);
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileSubtitleInput = document.querySelector("#profile-subtitle");
+const addNewCardButton = document.querySelector(".profile__add-button");
+const addCardCloseModalButton = addCardModal.querySelector(
+  "#modal-close-card-button"
+);
+const buttonPreview = document.querySelector(".modal__close-preview-button");
+
+
+const profileName = document.querySelector("#profile-title");
+const profileSubtitle = document.querySelector("#profile-subtitle");
+
+
+const cardTitleInput = addCardFormElement.querySelector(
+  ".modal__inputs_title"
+);
+const cardURLInput = addCardFormElement.querySelector(".modal__inputs_url");
+
+
+
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
+}
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
+
+function renderCard(cardData, wrapper) {
+  const cardElement = getCardElement(cardData);
+  wrapper.prepend(cardElement);
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardDescriptionEl = cardElement.querySelector(
+    ".card__description-text"
+  );
+  const likeButton = cardElement.querySelector(".card__button");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__button_active");
+  });
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImageEl.addEventListener("click", () => {
+    modalImagePreviewLink.src = cardImageEl.src;
+    modalImagePreviewLink.alt = cardImageEl.alt;
+    modalPreviewTitle.textContent = cardDescriptionEl.textContent;
+    openModal(modalPreviewImage);
+  });
+
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardDescriptionEl.textContent = cardData.name;
+  return cardElement;
+}
+
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileName.textContent = profileTitleInput.value;
+  profileSubtitle.textContent = profileSubtitleInput.value;
+  closeModal(profileEditModal);
+}
+
+function handleAddCardFormSubmit(e) {
+  e.preventDefault();
+  const name = cardTitleInput.value;
+  const link = cardURLInput.value;
+  renderCard({ name, link }, cardsWrap);
+  closeModal(addCardModal);
+  addCardFormElement.reset();
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileName.textContent;
+  profileSubtitleInput.value = profileSubtitle.textContent;
+  openModal(profileEditModal);
+});
+profileCloseModalButton.addEventListener("click", () =>
+  closeModal(profileEditModal)
+);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+buttonPreview.addEventListener("click", () => closeModal(modalPreviewImage));
+
+addNewCardButton.addEventListener("click", () => openModal(addCardModal));
+addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
+addCardCloseModalButton.addEventListener("click", () =>
+  closeModal(addCardModal)
+);
+
+initialCards.forEach((cardData) => renderCard(cardData, cardsWrap));
